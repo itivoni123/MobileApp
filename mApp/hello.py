f8fa78d875d906c64f94f253c058b17f086e8d18
Itai Tivoni <itai@ddaylabs.com>
2021-11-10 11:55:02 +0200

    done

--- mApp/hello.py
+++ mApp/hello.py
@@ -7,36 +7,16 @@ from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 
-Builder.load_file('my.kv')
+Builder.load_file('box.kv')
 
 
-class MyGridLayout(Widget):
-
-    name = ObjectProperty(None)
-    pizza = ObjectProperty(None)
-    color = ObjectProperty(None)
-
-    def press(self):
-
-        name = self.name.text
-        pizza = self.pizza.text
-        color = self.color.text
-        age = self.age.text
-
-        print(f'Hello {name}, you like {pizza} pizza, and your favorite color is {color} and your age is {age}')
-        # Print it to the screen
-        # self.add_widget(Label(text=f'Hello {name}, you like {pizza} pizza, and your favorite color is {color}!'))
-
-        # Clear the input boxes
-        self.name.text = ""
-        self.pizza.text = ""
-        self.color.text = ""
-        self.age.text = ""
+class MyLayout(Widget):
+    pass
 
 
 class DJApp(App):
     def build(self):
-        return MyGridLayout()
+        return MyLayout()
 # def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.from kivy.app import App
     # print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
